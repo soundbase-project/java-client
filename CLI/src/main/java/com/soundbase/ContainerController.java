1d1c371d53531f3dbe0f7a38010397394c32bfb5
Vagahbond <firroloni.yoni@gmail.com>
2020-05-05 12:54:36 +0200

    add navigation menu

--- CLI/src/main/java/com/soundbase/ContainerController.java
+++ CLI/src/main/java/com/soundbase/ContainerController.java
@@ -1,4 +1,13 @@
 package com.soundbase;
 
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class ContainerController {
+    @FXML
+    private void switchToSecondary() throws IOException {
+
+    }
+
 }
